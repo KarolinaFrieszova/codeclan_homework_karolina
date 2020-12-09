6f8c659af2f940487cb9b15319659549871b313d
Karolina Frieszova <karolinafriesz@gmail.com>
2020-12-09 21:50:01 +0000

    added answer to question 6

--- week_03/day_2/window_fun_join_sub.sql
+++ week_03/day_2/window_fun_join_sub.sql
@@ -12,7 +12,7 @@ SELECT
 	e.*,
 	pd.local_account_no,
 	pd.local_sort_code,
-	t.name
+	t.name AS team_name
 FROM (employees AS e LEFT JOIN pay_details AS pd ON e.id = pd.id)
 LEFT JOIN teams AS t
 ON e.team_id = t.id;
@@ -23,54 +23,60 @@ ON e.team_id = t.id;
 SELECT
 	e.first_name,
 	e.last_name,
-	t.name,
-	t.charge_cost > '80' AS charge_cost_greater_than_80
+	t.name AS team_name
 FROM employees AS e INNER JOIN teams AS t
 ON e.team_id = t.id
-ORDER BY e.last_name;
+WHERE CAST(t.charge_cost AS INT) > 80
+-- WHERE t.charge_cost::INT > 80
+ORDER BY e.last_name ASC NULLS LAST;
 
 /* 4. Breakdown the number of employees in each of the teams, including any teams without members. 
  * Order the table by increasing size of team. */
 
 SELECT
-	e.id,
-	e.first_name,
-	e.last_name,
-	t.name,
-	COUNT(e.id) AS num_of_emp_per_team
+	t.name AS team_name,
+	COUNT(e.id) AS num_employees
 FROM employees AS e RIGHT JOIN teams AS t
 ON e.team_id = t.id
-GROUP BY t.name, e.id
-ORDER BY COUNT(e.id);
+GROUP BY t.name
+ORDER BY COUNT(e.id) ASC;
 
 /* 5. The effective_salary of an employee is defined as their fte_hours multiplied by their salary. 
  * Get a table for each employee showing their id, first_name, last_name, fte_hours, salary and effective_salary, 
  * along with a running total of effective_salary with employees placed in ascending order of effective_salary. */
-------?
+
 SELECT
 	id,
 	first_name,
 	last_name,
 	fte_hours,
 	salary,
-	fte_hours * salary OVER (PARTITION BY id) AS effective_salary
-FROM employees
-ORDER BY effective_salary;
+	fte_hours * salary AS effective_salary,
+	SUM(fte_hours * salary) OVER (ORDER BY fte_hours * salary ASC NULLS LAST) AS running_tot_effective_salary
+FROM employees;
 
 /* 6. The total_day_charge of a team is defined as the charge_cost of the team multiplied 
- * by the number of employees in the team. Calculate the total_day_charge for each team.
- * If you GROUP BY teams.id, because it’s the primary key, you can SELECT any other column of teams that you want
- * (this is an exception to the rule that normally you can only SELECT a column that you GROUP BY).
- * You can also tackle this efficiently using a CTE. */
---- ?
+ * by the number of employees in the team. Calculate the total_day_charge for each team. */
+
+WITH team_sizes(id, size) AS (
 SELECT
-FROM
+	team_id,
+	COUNT(team_id)
+FROM employees
+GROUP BY team_id)
+SELECT
+	teams.name AS team_name,
+	teams.charge_cost::INT * team_sizes.size AS total_day_chart
+FROM teams INNER JOIN team_sizes
+ON teams.id = team_sizes.id
+
+/* 7. How would you amend your query from question 6 above to show only those teams with a total_day_charge greater than 5000? */
 
 
-/* How many of the employees serve on one or more committees? */
-/* how many employees serve per committees, if employee serves in one or more count
- */
----?
+
+/*  How many of the employees serve on one or more committees? */
+/* how many employees serve per committees, if employee serves in one or more count */
+
 SELECT DISTINCT(COUNT(employee_id))
 FROM employees_committees
 GROUP BY committee_id
